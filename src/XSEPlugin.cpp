3e0606df807052f920f07fa67135f3d36cbd0453
Mike Wyatt <wyatt.mike@gmail.com>
2022-11-05 01:37:33 -0300

    upgrade to clib-ng 3.5.6

--- src/XSEPlugin.cpp
+++ src/XSEPlugin.cpp
@@ -98,6 +98,8 @@ EXTERN_C [[maybe_unused]] __declspec(dllexport) constinit auto SKSEPlugin_Versio
 	v.PluginName("PluginName");
 	v.PluginVersion({ 1, 0, 0, 0 });
 	v.UsesAddressLibrary(true);
+	v.HasNoStructUse(true);
+	v.UsesStructsPost629(false);
 	return v;
 }();
 
