1910fa5eb700ba7924cef5e509d397e2af834ae6
Tim <15017472+doodlum@users.noreply.github.com>
2022-11-26 21:59:18 +0000

    1.6.629+ support

--- src/XSEPlugin.cpp
+++ src/XSEPlugin.cpp
@@ -1,11 +1,11 @@
 
-std::uintptr_t MainProperties;
+std::int64_t MainProperties;
 
 struct Hooks
 {
 	struct BSShaderRenderTargets_Create_MAIN
 	{
-		static void thunk(void* BSShaderRenderTargets, uint32_t TargetIndex, std::uintptr_t Properties)
+		static void thunk(void* BSShaderRenderTargets, uint32_t TargetIndex, std::int64_t Properties)
 		{
 			logger::info("Storing main render target information");
 			MainProperties = Properties;
@@ -16,7 +16,7 @@ struct Hooks
 
 	struct BSShaderRenderTargets_Create_LOCALMAP
 	{
-		static void thunk(void* BSShaderRenderTargets, uint32_t TargetIndex, [[maybe_unused]] std::uintptr_t Properties)
+		static void thunk(void* BSShaderRenderTargets, uint32_t TargetIndex, [[maybe_unused]] std::int64_t Properties)
 		{
 			logger::info("Patching LOCAL_MAP");
 			func(BSShaderRenderTargets, TargetIndex, MainProperties);
@@ -26,7 +26,7 @@ struct Hooks
 
 	struct BSShaderRenderTargets_Create_LOCALMAPSWAP
 	{
-		static void thunk(void* BSShaderRenderTargets, uint32_t TargetIndex, [[maybe_unused]] std::uintptr_t Properties)
+		static void thunk(void* BSShaderRenderTargets, uint32_t TargetIndex, [[maybe_unused]] std::int64_t Properties)
 		{
 			logger::info("Patching LOCAL_MAP_SWAP");
 			func(BSShaderRenderTargets, TargetIndex, MainProperties);
@@ -43,68 +43,7 @@ struct Hooks
 	}
 };
 
-void Init()
+void Load()
 {
 	Hooks::Install();
-}
-
-void InitializeLog()
-{
-#ifndef NDEBUG
-	auto sink = std::make_shared<spdlog::sinks::msvc_sink_mt>();
-#else
-	auto path = logger::log_directory();
-	if (!path) {
-		util::report_and_fail("Failed to find standard logging directory"sv);
-	}
-
-	*path /= fmt::format("{}.log"sv, Plugin::NAME);
-	auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
-#endif
-
-#ifndef NDEBUG
-	const auto level = spdlog::level::trace;
-#else
-	const auto level = spdlog::level::info;
-#endif
-
-	auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
-	log->set_level(level);
-	log->flush_on(level);
-
-	spdlog::set_default_logger(std::move(log));
-	spdlog::set_pattern("[%l] %v"s);
-}
-
-EXTERN_C [[maybe_unused]] __declspec(dllexport) bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
-{
-#ifndef NDEBUG
-	while (!IsDebuggerPresent()) {};
-#endif
-
-	InitializeLog();
-
-	logger::info("Loaded plugin");
-
-	SKSE::Init(a_skse);
-
-	Init();
-
-	return true;
-}
-
-EXTERN_C [[maybe_unused]] __declspec(dllexport) constinit auto SKSEPlugin_Version = []() noexcept {
-	SKSE::PluginVersionData v;
-	v.PluginName("PluginName");
-	v.PluginVersion({ 1, 0, 0, 0 });
-	v.UsesAddressLibrary(true);
-	return v;
-}();
-
-EXTERN_C [[maybe_unused]] __declspec(dllexport) bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface*, SKSE::PluginInfo* pluginInfo)
-{
-	pluginInfo->name = SKSEPlugin_Version.pluginName;
-	pluginInfo->infoVersion = SKSE::PluginInfo::kVersion;
-	pluginInfo->version = SKSEPlugin_Version.pluginVersion;
-	return true;
 }
