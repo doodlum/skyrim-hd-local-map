dd18af6673135b7bf591c8d7b0d61a68114af855
Tim <15017472+doodlum@users.noreply.github.com>
2024-09-24 20:54:23 +0100

    chore: modernised build system

--- include/PCH.h
+++ include/PCH.h
@@ -1,20 +1,11 @@
 #pragma once
 
 #pragma warning(push)
-#if defined(FALLOUT4)
-#	include "F4SE/F4SE.h"
-#	include "RE/Fallout.h"
-#	define SKSE F4SE
-#	define SKSEAPI F4SEAPI
-#	define SKSEPlugin_Load F4SEPlugin_Load
-#	define SKSEPlugin_Query F4SEPlugin_Query
-#	define RUNTIME RUNTIME_1_10_163
-#else
+
+#	define SKSE_SUPPORT_XBYAK
 #	include "RE/Skyrim.h"
 #	include "SKSE/SKSE.h"
-#endif
-
-#include <Windows.h>
+#	include <xbyak/xbyak.h>
 
 #ifdef NDEBUG
 #	include <spdlog/sinks/basic_file_sink.h>
@@ -23,6 +14,9 @@
 #endif
 
 #pragma warning(pop)
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
 
 using namespace std::literals;
 
@@ -34,23 +28,36 @@ namespace stl
 	void write_thunk_call(std::uintptr_t a_src)
 	{
 		SKSE::AllocTrampoline(14);
-
 		auto& trampoline = SKSE::GetTrampoline();
 		T::func = trampoline.write_call<5>(a_src, T::thunk);
 	}
 
-	template <class F, std::size_t idx, class T>
+	template <class F, size_t index, class T>
 	void write_vfunc()
 	{
-		REL::Relocation<std::uintptr_t> vtbl{ F::VTABLE[0] };
-		T::func = vtbl.write_vfunc(idx, T::thunk);
+		REL::Relocation<std::uintptr_t> vtbl{ F::VTABLE[index] };
+		T::func = vtbl.write_vfunc(T::size, T::thunk);
 	}
 
 	template <std::size_t idx, class T>
-	void write_vfunc(REL::ID id)
+	void write_vfunc(REL::VariantID id)
 	{
 		REL::Relocation<std::uintptr_t> vtbl{ id };
 		T::func = vtbl.write_vfunc(idx, T::thunk);
+	}
+
+	template <class T>
+	void write_thunk_jmp(std::uintptr_t a_src)
+	{
+		SKSE::AllocTrampoline(14);
+		auto& trampoline = SKSE::GetTrampoline();
+		T::func = trampoline.write_branch<5>(a_src, T::thunk);
+	}
+
+	template <class F, class T>
+	void write_vfunc()
+	{
+		write_vfunc<F, 0, T>();
 	}
 }
 
@@ -77,8 +84,8 @@ void InitializeLog()
 		util::report_and_fail("Failed to find standard logging directory"sv);
 	}
 
-	*path /= fmt::format("{}.log"sv, Plugin::NAME);
-	auto       sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
+	*path /= std::format("{}.log"sv, Plugin::NAME);
+	auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
 #endif
 
 #ifndef NDEBUG
@@ -89,16 +96,16 @@ void InitializeLog()
 
 	auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
 	log->set_level(level);
-	log->flush_on(level);
+	log->flush_on(spdlog::level::info);
 
 	spdlog::set_default_logger(std::move(log));
-	spdlog::set_pattern("[%l] %v"s);
+	spdlog::set_pattern("%v"s);
 }
 
-EXTERN_C [[maybe_unused]] __declspec(dllexport) bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
+extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
 {
 #ifndef NDEBUG
-	while (!IsDebuggerPresent()) {};
+	while (!REX::W32::IsDebuggerPresent()) {};
 #endif
 	InitializeLog();
 	logger::info("Loaded plugin");
@@ -107,7 +114,7 @@ EXTERN_C [[maybe_unused]] __declspec(dllexport) bool SKSEAPI SKSEPlugin_Load(con
 	return true;
 }
 
-EXTERN_C [[maybe_unused]] __declspec(dllexport) constinit auto SKSEPlugin_Version = []() noexcept {
+extern "C" DLLEXPORT constinit auto SKSEPlugin_Version = []() noexcept {
 	SKSE::PluginVersionData v;
 	v.PluginName(Plugin::NAME.data());
 	v.PluginVersion(Plugin::VERSION);
@@ -116,7 +123,7 @@ EXTERN_C [[maybe_unused]] __declspec(dllexport) constinit auto SKSEPlugin_Versio
 	return v;
 }();
 
-EXTERN_C [[maybe_unused]] __declspec(dllexport) bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface*, SKSE::PluginInfo* pluginInfo)
+extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface*, SKSE::PluginInfo* pluginInfo)
 {
 	pluginInfo->name = SKSEPlugin_Version.pluginName;
 	pluginInfo->infoVersion = SKSE::PluginInfo::kVersion;
